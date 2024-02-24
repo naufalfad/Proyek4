a91592e6ccce86f867bd59a2f012775cee99d476
naufalfad <naufalfad29@gmail.com>
2024-02-24 21:40:20 +0700

    Menambahkan file-file Flutter

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const LoginT = Color(0xFF5D1049);
+const appbarColor= Color.fromARGB(255, 101, 1, 114);
+const anima = Color.fromARGB(255, 101, 1, 114);
+const ULine = Color.fromARGB(255, 0, 0, 0);
+
+const Txt = Color.fromARGB(255, 0, 0, 0);
+
+const ErrorRed = Color(0xFFC5032B);
+
+const Tombol = Color.fromARGB(255, 3, 3, 3);
+const LoginBg = Color.fromARGB(255, 255, 253, 253);
